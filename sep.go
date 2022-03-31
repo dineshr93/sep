abb76a17a13bdc95cafb4165d65881fb8664d786
Ravi Dinesh <Dinesh.Ravi@elektrobit.com>
2022-03-31 17:15:00 +0200

    enabled concurrency

--- sep.go
+++ sep.go
@@ -7,9 +7,11 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	ossDir := "oss"
 	ossFile := "oss.txt"
 	propDir := "prop"
@@ -24,7 +26,8 @@ func main() {
 		os.Exit(3)
 	}
 	fmt.Println(folderPath)
-	// open file
+
+	// file handle for proprietary file
 	f, err := os.Open(ossFile)
 	if err != nil {
 		log.Fatal(err)
@@ -32,16 +35,20 @@ func main() {
 	// remember to close the file at the end of the program
 	defer f.Close()
 
+	// file handle for proprietary file
 	p, err := os.Open(propFile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// remember to close the file at the end of the program
 	defer p.Close()
+
+	// present working directory
 	// mydir, err := os.Getwd()
 	// if err != nil {
 	// 	fmt.Println(err)
 	// }
+
 	fullpropDir := folderPath + string(os.PathSeparator) + propDir
 	fullossDir := folderPath + string(os.PathSeparator) + ossDir
 	if _, err := os.Stat(fullpropDir); os.IsNotExist(err) {
@@ -61,36 +68,47 @@ func main() {
 
 	// read the file line by line using scanner
 	scanner1 := bufio.NewScanner(f)
-
-	for scanner1.Scan() {
-		if scanner1.Text() != "" {
-			fmt.Printf("%s\n", scanner1.Text())
-			oldLocation := folderPath + string(os.PathSeparator) + scanner1.Text()
-			newLocation := fullossDir + string(os.PathSeparator) + scanner1.Text()
-			err := os.Rename(oldLocation, newLocation)
-			if err != nil {
-				log.Fatal(err)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for scanner1.Scan() {
+			if scanner1.Text() != "" {
+				fmt.Printf("%s\n", scanner1.Text())
+				oldLocation := folderPath + string(os.PathSeparator) + scanner1.Text()
+				newLocation := fullossDir + string(os.PathSeparator) + scanner1.Text()
+				err := os.Rename(oldLocation, newLocation)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
-	}
-	if err := scanner1.Err(); err != nil {
-		log.Fatal(err)
-	}
+		if err := scanner1.Err(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
 	scanner := bufio.NewScanner(p)
-
-	for scanner.Scan() {
-		if scanner.Text() != "" {
-			fmt.Printf("%s\n", scanner.Text())
-			oldLocation := folderPath + string(os.PathSeparator) + scanner.Text()
-			newLocation := fullpropDir + string(os.PathSeparator) + scanner.Text()
-			err := os.Rename(oldLocation, newLocation)
-			if err != nil {
-				log.Fatal(err)
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for scanner.Scan() {
+			if scanner.Text() != "" {
+				fmt.Printf("%s\n", scanner.Text())
+				oldLocation := folderPath + string(os.PathSeparator) + scanner.Text()
+				newLocation := fullpropDir + string(os.PathSeparator) + scanner.Text()
+				err := os.Rename(oldLocation, newLocation)
+				if err != nil {
+					log.Fatal(err)
+				}
 			}
 		}
-	}
+	}()
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+	if err := scanner1.Err(); err != nil {
+		log.Fatal(err)
+	}
+	wg.Wait()
 }
